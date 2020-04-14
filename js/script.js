5ad4f593669f6e8f513a230482e10ef1a37f602b
Mohammed Al Safadi <43495034+mhmadsafadi@users.noreply.github.com>
2020-04-14 23:26:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ window.addEventListener("scroll", function(){
     var navbar = this.document.querySelector("nav");
     navbar.classList.toggle("togglenav", window.scrollY > 0);
 })
+
+
+const navSlide = () => {
+    const menuIcon = document.querySelector('.menu-icon');
+    const nav = document.querySelector('.ul-links');
+
+    menuIcon.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        menuIcon.classList.toggle('toggle');
+    });
+}
+navSlide();
